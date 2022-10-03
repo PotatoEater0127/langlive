06ed28f2481c3d3a94cabbfa5e123a260a358c4e
Henry Han <potatoEater0127@gmail.com>
2022-10-03 21:31:09 +0800

    feat: 初步切劃出倒數計時與抽獎名單的元件

---
+++ src/features/candidates/Candidates.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Candidates() {
+  return (
+    <div>
+      <h2>參與抽獎名單</h2>
+      <div>
+        <div>
+          <img />
+          <div>Jack</div>
+        </div>
+        <div>
+          <img />
+          <div>Tom</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/countdown/CountDown.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function CountDown() {
+  return (
+    <div>
+      <h2>抽獎時間</h2>
+      <input />
+      分鐘
+      <button>設定</button>
+      <div>00:00</div>
+    </div>
+  );
+}
