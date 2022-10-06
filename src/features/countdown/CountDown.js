8403f9b686bb24a94f65ce0df12b65d580694fd1
Henry Han <potatoEater0127@gmail.com>
2022-10-06 13:47:58 +0800

    feat: 建立HH:mm:ss的倒數計時:wq形式

--- src/features/countdown/CountDown.js
+++ src/features/countdown/CountDown.js
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrent, tick, selectCurrentTime } from "./countDownSlice";
+import { formatTime } from "./utils";
 
 export function CountDown() {
   const [inputTime, setInputTime] = useState(0);
   const dispatch = useDispatch();
   const currentTime = useSelector(selectCurrentTime);
-  const isReady = isFinite(inputTime) && inputTime > 0;
+
+  const isValidInput = isFinite(inputTime) && inputTime > 0;
+  const isCounting = currentTime > 0;
+  const isDisabled = !isValidInput || isCounting;
 
   useEffect(() => {
     // 以1秒鐘為倒數計時單位
     const tickUnit = 1000;
+    let timer = null;
     if (currentTime >= tickUnit) {
-      setTimeout(() => {
+      timer = setTimeout(() => {
         dispatch(tick(tickUnit));
       }, tickUnit);
     }
-  }, [currentTime]);
+    return () => clearTimeout(timer);
+  }, [currentTime, dispatch]);
 
   const onSet = () => {
-    dispatch(setCurrent(inputTime));
+    // 以1分鐘為輸入單位，轉換成毫秒
+    const unit = 60000;
+    setInputTime(0);
+    dispatch(setCurrent(inputTime * unit));
   };
 
   return (
@@ -28,14 +37,19 @@ export function CountDown() {
       <input
         type="number"
         min="0"
-        value={inputTime}
-        onChange={(e) => setInputTime(e.target.value)}
+        value={String(inputTime)} // 轉成字串以避免leading zeros，參考https://tinyurl.com/5cu2xsup
+        onChange={(e) => {
+          setInputTime(e.target.value);
+        }}
+        onBlur={({ target: { value } }) => {
+          setInputTime(Math.floor(value));
+        }}
       />
       分鐘
-      <button disabled={!isReady} onClick={onSet}>
+      <button disabled={isDisabled} onClick={onSet}>
         設定
       </button>
-      <div>{currentTime}</div>
+      <div>{formatTime(currentTime)}</div>
     </div>
   );
 }
---
+++ src/features/countdown/utils.js
@@ -0,0 +1,11 @@
+export function formatTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  const HH = String(hours % 24).padStart(2, "0");
+  const mm = String(minutes % 60).padStart(2, "0");
+  const ss = String(seconds % 60).padStart(2, "0");
+
+  return `${HH}:${mm}:${ss}`;
+}
