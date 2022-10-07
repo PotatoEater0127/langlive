432b5ecc9d99e0fcec3b5c7524ef58566fb2252e
Henry Han <potatoEater0127@gmail.com>
2022-10-07 14:07:26 +0800

    feat: 替抽獎結果頁面加上bulma style

--- src/features/lotteryResult/LotteryResult.js
+++ src/features/lotteryResult/LotteryResult.js
@@ -7,14 +7,30 @@ export function LotteryResult() {
   const dispatch = useDispatch();
   return (
     <div>
-      <h2>抽獎結果</h2>
-      <img
-        alt={`${winner.name} 的使用者圖片`}
-        src={winner.image}
-        style={{ width: "20%" }}
-      />
-      {winner.name}
-      <button onClick={() => dispatch(clearWinner())}>再來一次</button>
+      <h1 className="is-size-1 mt-2">抽獎結果</h1>
+      <figure>
+        <img
+          alt={`${winner.name} 的使用者圖片`}
+          src={winner.image}
+          style={{
+            maxWidth: "512px",
+            border: "1px solid",
+            borderColor: "#00d1b2",
+            borderRadius: "50%",
+          }}
+        />
+        <figcaption className="is-size-3">
+          <span className="is-size-4">{`編號：${winner.key}`}</span>
+          <br />
+          <span>{winner.name}</span>
+        </figcaption>
+      </figure>
+      <button
+        className="button is-primary"
+        onClick={() => dispatch(clearWinner())}
+      >
+        再來一次
+      </button>
     </div>
   );
 }
